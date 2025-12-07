ffea7dcb343617d5e1fb71ad05d0f1d5fb278900
25002755 <25002755@gateshead.ac.uk>
2025-12-07 21:20:39 +0000

    Add files via upload
    Upload python task

---
+++ task2_examples.py
@@ -0,0 +1,23 @@
+# Input example
+name = input("Enter your name: ")
+print("Hello", name)
+
+# Output example
+print("This is an example of output")
+
+# Variable example
+age = 15
+print(age)
+
+# List example + loop
+animals = ["cat", "dog", "bird"]
+for a in animals:
+    print(a)
+
+# Dictionary example
+book = {
+    "title": "Snow Day",
+    "pages": 120,
+    "author": "Sara"
+}
+print(book["title"])
